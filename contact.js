2cf0b15d932234668fc5053ad02b6feec5d598ce
Abdurahman Baktybek uulu <abdurahmanbaktybekuulu@MacBook-Air.local>
2024-02-10 03:15:04 -0800

    Just a little update

---
+++ contact.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var selectMenu = document.getElementById('contactMethod');
+    var inputEmail = document.getElementById('inputEmail');
+    var inputPhone = document.getElementById('inputPhone');
+
+    selectMenu.addEventListener('change', function() {
+        if (this.value === 'Email') {
+            inputEmail.style.display = 'block';
+            inputPhone.style.display = 'none';
+        } else if (this.value === 'Phone') {
+            inputPhone.style.display = 'block';
+            inputEmail.style.display = 'none';
+        }
+    });
+});
+
